7ee11a8f8baefc7f50505752d7dadc58fc41be73
Yash Modi <yash.modi@apps2fusion.net>
2021-12-02 20:19:35 +0530

    commit code

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Banner = () => (
-  <section
-    id="banner"
-    style={{
-      height: '100vh',
-      backgroundColor: 'grey',
-    }}
-  />
-);
+// let counter = 0;
+
+// LIfe Cycle method
+
+// Mounting
+
+// ->
+
+// Updating (Props or State)
+
+// Unmount
+
+// Error
+
+class Banner extends Component {
+  state = {
+    counter: 0,
+  };
+
+  incrementCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  decrementCounter = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
+
+  render() {
+    console.log('render Banner');
+    return (
+      <section
+        id="banner"
+        style={{
+          height: '100vh',
+          backgroundColor: 'grey',
+        }}
+      >
+        <h1>{this.props.title}</h1>
+        <h2>{this.state.counter}</h2>
+        <button type="button" onClick={this.incrementCounter}>
+          Increment Counter
+        </button>
+        <button type="button" onClick={this.decrementCounter}>
+          Decrement Counter
+        </button>
+      </section>
+    );
+  }
+}
+
+// const Banner = (props) => (
+//   <section
+//     id="banner"
+//     style={{
+//       height: '100vh',
+//       backgroundColor: 'grey',
+//     }}
+//   >
+//     {props.title}
+//   </section>
+// );
 
 export default Banner;
--- src/app.js
+++ src/app.js
@@ -4,10 +4,18 @@ import Header from './Header';
 import Banner from './Banner';
 import Footer from './Footer';
 
+// Function Component(stateless component)
+// -> Function Occupy Less memory
+// -> Only use for display UI base on props
+
+// Class Component (stateful Component)
+// -> if you want to write methods
+// -> base on event if you want to change UI using state
+
 const App = () => (
   <>
     <Header />
-    <Banner />
+    <Banner title="Hello Yagnesh" />
     <Footer />
   </>
 );
